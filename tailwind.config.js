f4b48f45fbf150aadbbda3d6ddfcedeca4f34a87
enftco <enft.co@gmail.com>
2022-07-21 23:04:38 +0800

    Add hero section and add images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      "sans": ["Poppins", "sans-serif"],
+      "mono": ["IBM Plex Mono", "monospace"],
+    },
+    colors: {
+      "dark": "#060707",
+      "primary": "#02C173",
+      "secondary": "#f0f0f0",
+    },
+    extend: {
+      keyframes: {
+        righ: {
+          "0%, 100%": { transform: "translateX(-10%)" },
+          "50%": { transform: "translateX(10%)" },
+        },
+      },
+      animation: {
+        "spin-slow": "spin 5s linear infinite",
+        "slit": "righ 2s infinite",
+      },
+    },
   },
   plugins: [],
 }
